5232a90aaac55e8f5778388bbdd6b3a41e26232a
SyedaMaryamZia <162296856+SyedaMaryamZia@users.noreply.github.com>
2024-04-21 23:20:03 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+function cars(manufacture:string,modelname:string,arbitrarynum:string,color:string,feature:string){
+    console.log(manufacture,modelname,arbitrarynum,color,feature)
+}
+cars("Honda","2023","2111vvn","red","airbags")
